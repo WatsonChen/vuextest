29754ada34df23de93b3e2a3fa87e20449705178
watson <watson@okmaster.com>
2021-01-14 12:27:25 +0800

    test basic vuex

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,18 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    count: 10
+  },
+  mutations: {
+    increment_mutation(state,counting){
+      state.count += counting
+    }
+  },
+  actions: {
+    increment_action(context, counting){
+      context.commit('increment_mutation',counting)
+    }
+  },
   modules: {}
 });
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,26 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>count: {{ count }}</p>
+    <button @click="dispathCount">Click to add</button>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      counting: 5
+    }
+  },
+  computed: {
+    count() {
+      return this.$store.state.count 
+    }
+  },
+  methods: {
+    dispathCount() {
+      this.$store.dispatch('increment_action',this.counting)
+    }
+  },
+}
+</script>
